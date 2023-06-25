7b8b510c7d2d3f6ea55c4a827140a81e007b8976
RolandoDrRobot <rolando.arguello.email@gmail.com>
2023-06-25 07:59:27 -0600

    Safe place

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 const express = require('express');
-const passport = require('./services/passport');
 const cors = require('cors');
 import { Request, Response } from 'express';
 import { getPurchases, createPurchase, removePurchase } from './services/purchaseCRUD'
@@ -9,12 +8,6 @@ const benKimServer = express();
 benKimServer.use(cors());
 benKimServer.use(express.json());
 benKimServer.use(express.urlencoded({ extended: false }));
-benKimServer.use(passport.initialize());
-benKimServer.use(passport.session());
-benKimServer.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-benKimServer.get('/auth/google/callback', passport.authenticate('google'), { successRedirect: '/' })
-
 benKimServer.listen(443, () => {
   console.log(`Server on port 443`);
 });
