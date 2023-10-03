52521598479ca75b13c043825185710c4cbf3721
RolandoDrRobot <rolando.arguello.email@gmail.com>
2023-10-03 00:15:32 -0600

    Trying to make the port work

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
