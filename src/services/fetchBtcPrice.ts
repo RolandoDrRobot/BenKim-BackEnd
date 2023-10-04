e1d5bc5a4d385f7264d082e162ddc02471410373
RolandoDrRobot <rolando.arguello.email@gmail.com>
2023-10-03 19:26:59 -0600

    Remove axios

--- src/services/fetchBtcPrice.ts
+++ src/services/fetchBtcPrice.ts
@@ -1,10 +1,12 @@
-const axios = require('axios');
 const apiUrl = 'https://api.coincap.io/v2/assets/bitcoin';
 
 const fetchBtcPrice = async () => {
   try {
-    const response = await axios.get(apiUrl);
-    const btcPrice = response.data.data.priceUsd;
+    const response = await fetch(apiUrl);
+    if (!response.ok) throw new Error('Error al hacer la solicitud a la API de coincap.io');
+
+    const data = await response.json();
+    const btcPrice = data.data.priceUsd;
     return btcPrice;
   } catch (error) {
     console.error('Error al hacer la solicitud a la API de coincap.io:');
